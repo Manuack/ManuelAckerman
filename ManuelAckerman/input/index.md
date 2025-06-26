7182cc8530abc3c4715d021c011d2bacb749b7c0
Manuel Ackerman Barboni <ma271521@fi365.ort.edu.uy>
2025-06-25 22:37:48 -0300

    proyect new start with statiq

---
+++ ManuelAckerman/input/index.md
@@ -0,0 +1,7 @@
+---
+Title: "Mi Sitio Personal"
+---
+
+# Bienvenido a la web de Manuel Ackerman
+
+Próximamente encontrarás aquí mis historias y fotos.
---
+++ PageGenerator/PageGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Statiq.Markdown;
+
+namespace PageGenerator
+{
+    public class PageGenerator
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateDefault(args)
+                .BuildPipeline("Render Markdown", builder => builder
+                    .WithInputReadFiles("*.md")
+                    .WithProcessModules(new RenderMarkdown())
+                    .WithOutputWriteFiles(".html"))
+                .RunAsync();
+    }
+}
