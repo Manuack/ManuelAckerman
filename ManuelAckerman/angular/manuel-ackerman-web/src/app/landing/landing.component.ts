933948b457be256290e0a07761060b5867c02d56
Manuack <manuack23@gmail.com>
2025-08-02 11:22:17 -0300

    Angular landing page v1

---
+++ ManuelAckerman/angular/manuel-ackerman-web/src/app/landing/landing.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent {
+  parallaxTransform = 'translate(0,0)';
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const x = (e.clientX / innerWidth - 0.5) * 20;
+    const y = (e.clientY / innerHeight - 0.5) * 20;
+    this.parallaxTransform = `translate(${x}px, ${y}px)`;
+  }
+
+  onInstagramClick() {
+    window.open('https://www.instagram.com/manu_el_ackerman', '_blank');
+  }
+}
